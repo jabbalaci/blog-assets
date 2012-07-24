040dad0f15e1ad1fd400bb3a7a816ab3091fb117
Jabba Laci <jabba.laci@gmail.com>
2012-07-24 14:26:02 +0200

    L

--- 20120517-Github-Python-Projects/ghpp.py
+++ 20120517-Github-Python-Projects/ghpp.py
@@ -25,11 +25,9 @@ def remove_non_ascii(text):
     return ''.join(c for c in text if ord(c) < 128)
 
 def process(r):
-    print '{o}/{n}   (watchers: {w}, forks: {f}, updated: {u})'.format(o=r['owner'], 
-                                                                       n=r['name'], 
-                                                                       w=r['watchers'], 
-                                                                       f=r['forks'],
-                                                                       u=r['pushed'][:10])
+    print '{o}/{n}   (watchers: {w}, forks: {f}, updated: {u})' \
+        .format(o=r['owner'], n=r['name'], w=r['watchers'], 
+                f=r['forks'], u=r['pushed'][:10])
     desc = '\n'.join(textwrap.wrap(remove_non_ascii(r['description']), WIDTH))
     print '{d}'.format(d=desc)
     print '{url}'.format(url=r['url']),
